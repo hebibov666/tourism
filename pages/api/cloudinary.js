64a97ee137c38e2ad9b77e84894302bc82b4e929
hebibov666 <asuslap503@gmail.com>
2024-02-17 10:55:20 +0300

    Request Pending error

---
+++ pages/api/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary=require("cloudinary").v2;
+cloudinary.config({ 
+    cloud_name: 'dp4enwbxa', 
+    api_key: '274421476858639', 
+    api_secret: 'r2nBcNTUzOwBJoHhefhBKJWAVVY' 
+  });
+
+  module.exports=cloudinary
